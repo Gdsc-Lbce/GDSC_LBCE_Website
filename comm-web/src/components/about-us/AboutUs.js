b1e598691e5dbdae10d96e3f23206e21ae1f2465
jay-io <jay.dnb@outlook.in>
2021-12-27 22:39:14 +0530

    Add segregated CSS files

--- comm-web/src/components/about-us/AboutUs.js
+++ comm-web/src/components/about-us/AboutUs.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import WavyCurve from "./wave.svg";
+import "./AboutUs.css";
 import Rocket from "./rocket.svg";
 import Collab from "./collaborating.svg";
 import AOS from "aos";
@@ -29,34 +29,34 @@ function AboutUs() {
     }, []);
 
     return (
-        <div id="about-us-div" style={{position: "absolute", top: `${window.innerHeight - 60}px`, display: "block", width: "100%"}}>
-            <div id="w-curve" style={{transition: "all 3s ease-out", overflowY: "hidden", backgroundRepeat: "no-repeat", backgroundImage: "url(" + WavyCurve + ")", backgroundPosition: "50% 90%", backgroundSize: "3000px 80px", width: `100%`, height: "85px"}} />
-            <div style={{display: "flex", flexDirection: "column", backgroundColor: "rgba(34, 32, 39, 1)", width: `100%`, paddingTop: "10px"}}>
-                <div data-aos="fade-right" style={{flex: "1 1 2%", display: "flex", paddingLeft: "50px", paddingBottom: "50px"}}>
-                    <span style={{textAlign: "left", font: "Ubuntu Mono", fontSize: "25px", fontWeight: "600", color: "rgb(74, 74, 75)"}}>LBCE /&nbsp;</span>
-                    <span style={{textAlign: "left", font: "Ubuntu Mono", fontSize: "25px", fontWeight: "600", color: "rgb(37, 169, 242)"}}>Google Developer Student Club</span>                
+        <div id="about-us-div" style={{top: `${window.innerHeight - 60}px`}}>
+            <div id="w-curve" />
+            <div id="about-us-bg">
+                <div id="about-us-title">
+                    <span id="about-us-title-lbce">LBCE /&nbsp;</span>
+                    <span id="about-us-title-gdsc">Google Developer Student Club</span>                
                 </div>
-                <div style={{flex: "1 1 49%", paddingLeft: "50px", display: "flex", paddingBottom: "60px"}}>
-                    <div style={{width: "45%", paddingTop: "10px"}}>
+                <div id="about-us-p-div">
+                    <div id="about-us-p-svg">
                         <img data-aos="zoom-in-up" src={Rocket} alt="" width="80%" />
                     </div>
-                    <div style={{display: "flex", flexDirection: "column", width: "45%", textAlign: "left", padding: "30px"}}>
-                        <div style={{flex: "1 1 48%"}} />
-                        <p data-aos="fade-up" data-aos-offset="50" style={{font: "Montserrat", fontSize: "15px", fontWeight: "600", color: "rgba(255, 255, 255, .8)", margin: "0"}}>WHO ARE WE?</p>
-                        <p data-aos="fade-up" data-aos-offset="50" style={{font: "Montserrat", fontSize: "30px", fontWeight: "700", color: "white", marginTop: "5px", marginBottom: "5px"}}>{"GDSC LBCE Chapter"}</p>
-                        <p data-aos="fade-up" data-aos-offset="50" style={{font: "Montserrat", fontSize: "15px", fontWeight: "400", color: "rgba(255, 255, 255, .6)", margin: "0"}}>{WHO_ARE_WE}</p>
-                        <div style={{flex: "1 1 48%"}} />
+                    <div id="about-us-p-content-div">
+                        <div id="about-us-p-content-spacerflex" />
+                        <p id="about-us-p-1" data-aos="fade-up" data-aos-offset="50">WHO ARE WE?</p>
+                        <p id="about-us-p-2" data-aos="fade-up" data-aos-offset="50">{"GDSC LBCE Chapter"}</p>
+                        <p id="about-us-p-3" data-aos="fade-up" data-aos-offset="50">{WHO_ARE_WE}</p>
+                        <div id="about-us-p-content-spacerflex" />
                     </div>
                 </div>
-                <div style={{flex: "1 1 49%", paddingLeft: "50px", display: "flex", paddingBottom: "60px"}}>
-                    <div style={{display: "flex", flexDirection: "column", width: "45%", textAlign: "left", padding: "30px"}}>
-                        <div style={{flex: "1 1 48%"}} />
-                        <p data-aos="fade-up" data-aos-offset="50" style={{font: "Montserrat", fontSize: "15px", fontWeight: "600", color: "rgba(255, 255, 255, .8)", margin: "0"}}>WHAT DO WE DO?</p>
-                        <p data-aos="fade-up" data-aos-offset="100" style={{font: "Montserrat", fontSize: "30px", fontWeight: "700", color: "white", marginTop: "5px", marginBottom: "5px"}}>“Bridging the gap<br />between theory and practice”</p>
-                        <p data-aos="fade-up" data-aos-offset="150" style={{font: "Montserrat", fontSize: "15px", fontWeight: "400", color: "rgba(255, 255, 255, .6)", margin: "0"}}>{WHAT_DO_WE_DO}</p>
-                        <div style={{flex: "1 1 48%"}} />
+                <div id="about-us-p-div">
+                    <div id="about-us-p-content-div">
+                        <div id="about-us-p-content-spacerflex" />
+                        <p id="about-us-p-1" data-aos="fade-up" data-aos-offset="50">WHAT DO WE DO?</p>
+                        <p id="about-us-p-2" data-aos="fade-up" data-aos-offset="100">“Bridging the gap<br />between theory and practice”</p>
+                        <p id="about-us-p-3" data-aos="fade-up" data-aos-offset="150">{WHAT_DO_WE_DO}</p>
+                        <div id="about-us-p-content-spacerflex" />
                     </div>
-                    <div style={{width: "45%"}}>
+                    <div id="about-us-p-svg">
                         <img data-aos="zoom-in-up" src={Collab} alt="" width="80%" />
                     </div>
                 </div>
--- comm-web/src/components/banner/Banner.js
+++ comm-web/src/components/banner/Banner.js
@@ -1,121 +1,57 @@
-import BannerBackground from "./codecode.jpg";
-import ArrowGradient from "./ArrowGradient.svg"
 import GdscMono from "./gdscMono.png";
 import Twitter from "./twitter.png";
 import LinkedIn from "./linkedin.png";
 import Github from "./github.png";
-
-const BANNER_HEIGHT = window.innerHeight;
-const MIN_CONTENT_WIDTH = 1024;
-const MAX_CONTENT_WIDTH = 2560;
-const SCROLLBAR_WIDTH = 0;
-
-const browserZoom = window.devicePixelRatio;
-const cssResolvedWidth = Math.round(window.screen.width - SCROLLBAR_WIDTH);
-var contentWidth;
-
-const bannerDiv = {
-    display: "flex",
-    position: "fixed",
-    top: "0px",
-    flexDirection: "column",
-    textAlign: "left",
-    backgroundImage: "url(" + ArrowGradient + "), url(" + BannerBackground + ")",
-    backgroundSize: "contain, cover",
-    backgroundPosition: "right, center",
-    backgroundRepeat: "no-repeat",
-};
-
-const bannerTextDiv0 = {
-    display: "flex",
-    flexDirection: "column",
-    flex: "auto",
-    padding: "0 0 0 6%",
-    color: "white",
-}
-
-const bannerTextDiv1 = {
-    display: "flex",
-    flexDirection: "column",
-    flex: "1 1 1%",
-    padding: "0 0 0 6%",
-    color: "white",
-};
-
-const bannerTextDiv2 = {
-    display: "flex",
-    flexDirection: "column",
-    flex: "auto",
-    padding: "0 0 0 6%",
-    color: "white",
-    verticalAlign: "bottom",
-}
-
-if (cssResolvedWidth <= MIN_CONTENT_WIDTH) {
-    bannerDiv.overflowX = "scroll";
-    contentWidth = MIN_CONTENT_WIDTH;
-    bannerDiv.backgroundSize = `${0.20 * contentWidth}px ${BANNER_HEIGHT}px, ${contentWidth}px auto`;
-} else if (cssResolvedWidth >= MAX_CONTENT_WIDTH) {
-    contentWidth = MAX_CONTENT_WIDTH;
-    bannerDiv.backgroundSize = `${0.30 * contentWidth}px ${BANNER_HEIGHT}px, ${contentWidth}px auto`;
-} else {
-    bannerDiv.backgroundSize = "cover auto";
-    contentWidth = cssResolvedWidth;
-    bannerDiv.backgroundSize = `${0.45 * contentWidth}px ${BANNER_HEIGHT}px, ${contentWidth}px auto`;
-    bannerDiv.overflowX = "hidden";
-};
-
-bannerDiv["height"] = `100%`;
-bannerDiv["width"] = `100%`;
+import "./Banner.css";
 
 function Banner() {
     return (
-        <div style={bannerDiv}>
-            <div style={bannerTextDiv0}>
-                <div style={{flex: "auto"}} />
+        <div id="banner-div">
+            <div id="banner-div-1">
+                <div id="banner-div-1-spacerflex-1" />
                 <div style={{display: "flex", flexDirection: "column", flex: "auto"}}>
-                    <div style={{flex: "1 1 96%"}} />
-                    <div style={{flex: "1 1 4%"}}>
+                    <div id="banner-div-1-spacerflex-2" />
+                    <div id="banner-div-1-content-div">
                         &nbsp;
                         <img src={GdscMono} alt="" height="13em" />
-                        <span style={{font: "PT Sans", fontSize: "1.2em", fontWeight: "900"}}>
+                        <span id="banner-div-1-content-gdsc">
                             &nbsp; Google Developer Student Club<br />
                         </span>
                     </div>
                 </div>
             </div>
-            <div style={bannerTextDiv1}>
-                <div style={{flex: "auto"}} />
-                <div style={{display: "flex", flexDirection: "column", flex: "auto"}}>
-                    <span style={{font: "Montserrat", fontSize: `${Math.round(1 / browserZoom * 4.5)}em`, fontWeight: "700"}}>
+            <div id="banner-div-2">
+                <div id="banner-div-2-spacerflex-1" />
+                <div id="banner-div-2-content-div">
+                    <span id="banner-div-2-content-drlbce">
                         Dr. Lankapalli<br />
                         Bullayya College<br />
                         of Engineering
                     </span>
                 </div>
-                <div style={{flex: "auto"}} />
+                <div id="banner-div-2-spacerflex-2" />
             </div>
-            <div style={bannerTextDiv2}>
-                <div style={{display: "flex", flex: "1 1 60%"}}>
-                    <div style={{flex: "1 1 80%"}}/>
-                    <div style={{display: "flex", flexDirection: "column", flex: "1 1 20%"}}>
-                        <div style={{flex: "1 1 69%"}} />
-                        <div style={{display: "flex", flex: "1 1 1%"}}>
-                            <div style={{flex: "1 1 20%"}}>
+            <div id="banner-div-3">
+                <div id="banner-div-3-div-socials">
+                    <div id="banner-div-3-div-socials-spacerflex-1"/>
+                    <div id="banner-div-3-div-socials-div-socials">
+                        <div id="banner-div-3-div-socials-spacerflex-2" />
+                        <div id="banner-div-3-div-socials-div-socials-div-socials">
+                            <div className="banner-div-3-div-socials-emblem">
                                 <img src={LinkedIn} alt="" height="25em" />
                             </div>
-                            <div style={{flex: "1 1 20%"}}>
+                            <div className="banner-div-3-div-socials-emblem">
                                 <img src={Twitter} alt="" height="25em" />
                             </div>
-                            <div style={{flex: "1 1 20%"}}>
+                            <div className="banner-div-3-div-socials-emblem">
                                 <img src={Github} alt="" height="25em" />
                             </div>
-                            <div style={{flex: "1 1 31%"}} />
+                            <div id="banner-div-3-div-socials-spacerflex-3" />
                         </div>
                     </div>
                 </div>
-                <div style={{flex: "1 1 40%"}}>
-                    <span style={{font: "Montserrat", fontSize: `${1 / browserZoom * 0.8}em`}}>
+                <div id="banner-div-3-div-powered">
+                    <span id="banner-div-3-div-powered-content">
                         &nbsp; Powered by <b>Google Developers</b><br />
                     </span>
                 </div>
