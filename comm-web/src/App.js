3274016da5f568fd932ad32d22e76bf7d74b6a61
jay-io <jay.dnb@outlook.in>
2021-12-27 17:24:32 +0530

    Merge manually with Gdsc-Lbce/GDSC_LBCE_Website

--- comm-web/src/App.js
+++ comm-web/src/App.js
@@ -1,14 +1,39 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Accordion from './components/FAQ/accordion';
+import { accordionData } from './components/FAQ/data';
+import accordionstyle from './components/FAQ/accordionstyle.css';
 import Banner from './components/banner/Banner';
-import AboutUs from './components/about-us/AboutUs'
+import AboutUs from './components/about-us/AboutUs';
 
-function App() {
-    return (
-        <div className="App">
-            <Banner />
-            <AboutUs />
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <Router className="App">
+    <Banner />
+    <Navbar />
+    <AboutUs />
+    <div>
+      <h1 id='faqtitle'>Frequently Asked Questions</h1>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div> 
+    </div>
+    </Router>
+    //  {/* <div>
+    //   <h1>Frequently Asked Questions</h1>
+    //   <div className="accordion">
+    //     {accordionData.map(({ title, content }) => (
+    //       <Accordion title={title} content={content} />
+    //     ))}
+    //   </div> 
+    // </div> */}
+    
+  );
+};
 
 export default App;
+
