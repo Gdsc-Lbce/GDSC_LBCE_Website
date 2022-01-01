9c0dddf05c3285266dd2fb4bc5a84afc09ee0891
Aman <amantechmsy@gmail.com>
2022-01-01 23:49:09 +0530

    Add Events Section

--- comm-web/src/App.js
+++ comm-web/src/App.js
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Accordion from './components/FAQ/accordion';
 import { accordionData } from './components/FAQ/data';
 import accordionstyle from './components/FAQ/accordionstyle.css';
+import Section from './components/Section/Section'
 
 const App = () => {
   return (
     <Router>
-    <Navbar /> 
+    <Navbar />
+    <Section /> 
     <div>
       <h1 id='faqtitle'>Frequently Asked Questions</h1>
       <div className="accordion">
@@ -19,15 +21,6 @@ const App = () => {
       </div> 
     </div>
     </Router>
-    //  {/* <div>
-    //   <h1>Frequently Asked Questions</h1>
-    //   <div className="accordion">
-    //     {accordionData.map(({ title, content }) => (
-    //       <Accordion title={title} content={content} />
-    //     ))}
-    //   </div> 
-    // </div> */}
-    
   );
 };
 
---
+++ comm-web/src/components/Section/SectionData.js
@@ -0,0 +1,19 @@
+import img1 from "../../images/tmp-img.jpg"
+
+export const sectionData = [
+    {
+        title: `GDSC-LBCE 2020`,
+        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, blanditiis!`,
+        imgUrl: img1
+    },
+    {
+        title: `GDSC-LBCE 2021`,
+        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, blanditiis!`,
+        imgUrl: img1
+    },
+    {
+        title: `GDSC-LBCE 2022`,
+        content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, blanditiis!`,
+        imgUrl: img1
+    }
+];
